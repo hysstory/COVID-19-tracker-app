e4d0eaabfa01cb3cebd05d043b00cf41c1a82c10
hysstory <dyhan92@gmail.com>
2020-07-29 20:30:35 -0400

    updates

--- src/main/java/com/hysstory/covidtracker/controllers/HomeController.java
+++ src/main/java/com/hysstory/covidtracker/controllers/HomeController.java
@@ -1,10 +1,16 @@
 package com.hysstory.covidtracker.controllers;
 
+import com.hysstory.covidtracker.models.LocationStats;
 import com.hysstory.covidtracker.services.CovidDataService;
+import jdk.jshell.execution.LoaderDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
 
 @Controller
 public class HomeController {
@@ -14,7 +20,14 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("stats", covidDataService.getAllStats());
+        List<LocationStats> stats = covidDataService.getAllStats();
+        int totalCases = stats.stream().mapToInt(LocationStats::getCases).sum();
+        String numberFormat =
+                NumberFormat
+                        .getNumberInstance(Locale.US)
+                        .format(totalCases);
+        model.addAttribute("stats", stats);
+        model.addAttribute("totalCases", numberFormat);
         return "index";
     }
 }
